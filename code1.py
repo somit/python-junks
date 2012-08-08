2f55fe021a92679fce3cf506ac00577c49d46e61
somithashedin <somit@hashedin.com>
2012-08-09 02:11:01 +0530

    first steps

---
+++ code1.py
@@ -0,0 +1,4 @@
+print 'Hello World'
+name= raw_input('What is your name?')
+print "Hi, %s" %name
+
---
+++ code2.py
@@ -0,0 +1,6 @@
+friends = ['john','tim','gajodhar']
+for i, name in enumerate(friends):
+	print "iteration {iteration} is {name} ".format(iteration=i, name=name)
+
+
+
